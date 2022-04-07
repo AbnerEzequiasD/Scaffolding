3e85d63b6eea6186b13ba075a137915f405fe99b
AbnerEzequiasD <90418131+AbnerEzequiasD@users.noreply.github.com>
2022-04-06 20:24:38 -0400

    Add files via upload

---
+++ DSFEBAPR2022.Win32/LoginForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSFEBAPR2022.Win32
+{
+    public partial class LoginForm : Form
+    {
+        DSFEBAPR2022.BO.DSFEBABR2022Entities db = new BO.DSFEBABR2022Entities();
+        private string ExecutePath = string.Empty;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            ExecutePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            getCredentials();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            DoLogin();
+        }
+
+        private void DoLogin()
+        {
+            var pwd = BO.Helpers.Utils.MD5Hash(txtClave.Text);
+            var user = db.Usuario.FirstOrDefault(x => x.Usuario1 == txtUsuario.Text.Trim() && x.Clave == pwd);
+            if (user == null)
+            {
+                MessageBox.Show("Credenciales Invalidas.");
+                return;
+            }
+
+            saveCredentials();
+
+            var oMainForm = new MainForm();
+            oMainForm.Show();
+
+            this.Hide();
+        }
+
+        private void saveCredentials()
+        {
+            if (chkRecordar.Checked)
+            {
+                StreamWriter sw = new StreamWriter(ExecutePath + "\\config.txt");
+                sw.WriteLine(txtUsuario.Text);
+                sw.WriteLine(BO.Helpers.Utils.MD5Hash(txtClave.Text));
+                sw.Close();
+            }
+            else
+            {
+                File.Delete(ExecutePath + "\\config.txt");
+            }
+        }
+
+        private void getCredentials()
+        {
+            if (File.Exists(ExecutePath + "\\config.txt"))
+            {
+                chkRecordar.Checked = true;
+                String line;
+                StreamReader sr = new StreamReader(ExecutePath + "\\config.txt");
+                line = sr.ReadLine();
+
+                txtUsuario.Text = line;
+                while (line != null)
+                {
+                    txtClave.Text = line;
+
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+        }
+    }
+}
---
+++ DSFEBAPR2022.Win32/MainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSFEBAPR2022.Win32
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void usuariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var oForm = new UserForm();
+            oForm.MdiParent = this;
+            oForm.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Estas seguro que deseas salir del sistema.", "DESARROLLO DE SOFTWARE", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+            if (dr == DialogResult.Yes)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        private void cerrarSesiónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var oLogin = new LoginForm();
+            oLogin.Show();
+            this.Hide();
+        }
+
+        private void ocupacionesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var oForm = new OcupationForm();
+            oForm.MdiParent = this;
+            oForm.Show();
+        }
+
+        private void cascadaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void iconosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            var formControl = (Form)ActiveMdiChild;
+            if (formControl != null)
+            {
+                ((MyInterface)formControl).Save();
+            }
+        }
+    }
+}
